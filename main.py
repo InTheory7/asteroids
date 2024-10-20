a4f0ec8003e9837289f043b3b6716bab1e7b6b7c
InTheory7 <joshuacollins999@gmail.com>
2024-10-20 16:31:37 +0100

    Added the ability to exit the game

--- main.py
+++ main.py
@@ -4,17 +4,23 @@ from constants import * # Or `from constants import *`
 def main():
     # Initialize pygame modules:
     pygame.init()
+    # Print CLI starting messages:
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     # Create the GUI window:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     # Game Loop
     while True:
+        # Exit the game if the GUI window is closed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill((0,0,0))    # Fill the screen with black (takes a tuple of RGB values).
         pygame.display.flip()   # Update the screen
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
